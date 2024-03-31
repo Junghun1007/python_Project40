a4bf3fb8249bb384b08481de0db688b0c386e49b
Junghun1007 <dhwjdgnsgns@gmail.com>
2024-03-31 23:18:35 +0900

    thread function

--- exchange_rate.py
+++ exchange_rate.py
@@ -1,9 +1,17 @@
-from currency_converter import CurrencyConverter
-import ssl
+import requests
+from bs4 import BeautifulSoup
 
-ssl._create_default_https_context = ssl._create_unverified_context
+def get_exchange_rate(target1, target2):
+    headers = {
+        'User-Agent' : 'Mozilla/5.0',
+        'Content-Type' : 'text/html; charset-utf-8'
+    }
+    response = requests.get("https://kr.investing.com/currencies/{}-{}".format(target1,
+                                                                              target2), headers=headers)
+    content = BeautifulSoup(response.content, 'html.parser')
+    containers = content.find('span',{'data-test':'instrument-price-last'})
+    print(containers.text)
 
-cc = CurrencyConverter('http://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip')
+get_exchange_rate('usd','krw')
 
-print(cc.convert(1,'USD','KRW'))
 
